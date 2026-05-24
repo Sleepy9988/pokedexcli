a052ba61f22e6c670821441529dadf33017cdadb
Sleepy9988 <s.mixner@gmail.com>
2026-05-24 19:21:52 +0200

    implemented REPL logic

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func main(){
-	fmt.Println("Hello, World!")
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if scanner.Err() != nil {
+		fmt.Printf("error: %s\n", scanner.Err())
+	}
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		for scanner.Scan() {
+			user_input := scanner.Text()
+			cleaned_input := cleanInput(user_input)
+			fmt.Printf("Your command was: %s\n", cleaned_input[0])
+			break
+		}
+	}
 }
