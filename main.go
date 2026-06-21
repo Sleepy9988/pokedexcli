02e3a7a533f06633c01839f22761a7fb87af6c2e
Sleepy9988 <s.mixner@gmail.com>
2026-06-21 17:07:03 +0200

    added catch command

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
 	c := &config{
 		pokeapiClient: pokeClient,
+		caughtPokemon: map[string]pokeapi.Pokemon{},
 	}
 
 	startRepl(c, nil)
